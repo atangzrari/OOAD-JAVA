e4f04c9d2cf5185ca8671692a3071ca6b04ecaed
A.Z.Rari <cse24-090@thuto.bac.ac.bw>
2025-11-29 08:29:28 +0200

    Delete src/main/java/model directory

--- src/main/java/model/interfaces/AccountOperations.java
+++
@@ -1,7 +0,0 @@
-package model.interfaces;
-
-public interface AccountOperations {
-    boolean withdraw(double amount);
-    double deposit(double amount);
-    double getBalance();
-}
--- src/main/java/model/interfaces/CustomerOperations.java
+++
@@ -1,9 +0,0 @@
-package model.interfaces;
-
-import model.accounts.Account;
-import java.util.List;
-
-public interface CustomerOperations {
-    Account openAccount(String accountType, double initialDeposit);
-    List<Account> getAccounts();
-}
